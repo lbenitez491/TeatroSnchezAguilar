9ecd8b92440edb1b4b627570dd1e0354fb593de4
Luis Benítez <contacto@lbenitez.net>
2015-02-06 11:12:20 -0300

    Agrego repositorio del app de Teatro SA
    Maqueta

---
+++ .module/src/main/java/lb/app/teatrosnchezaguilar/module/MainActivity.java
@@ -0,0 +1,56 @@
+package lb.app.teatrosnchezaguilar.module;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent();
+                i.putExtra("nombre","luis");
+                i.setClass(MainActivity.this, SplashActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.action_quienes_somos:
+                Log.i("ActionBar", "Guardar!");;
+                return true;
+            case R.id.action_programacion:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            case R.id.action_calendario_eventos:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            case R.id.action_como_llegar:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            case R.id.action_contactenos:
+                Log.i("ActionBar", "Settings!");;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ .module/src/main/java/lb/app/teatrosnchezaguilar/module/SplashActivity.java
@@ -0,0 +1,39 @@
+package lb.app.teatrosnchezaguilar.module;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    // Duración en milisegundos que se mostrará el splash
+    private final int DURACION_SPLASH = 3000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Quitamos barra de titulo de la aplicacion
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Quitamos barra de notificaciones
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Tenemos una plantilla llamada splash.xml donde mostraremos la información que queramos (logotipo, etc.)
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            ;
+        }, DURACION_SPLASH);
+    }
+}
